c84c5470618cb4ce0f63f4e53617606d4f62b655
Muhammad Ismail <53092250+ismailkhan4@users.noreply.github.com>
2025-07-17 02:18:49 +0500

    Consolidate all styles into globals.css

--- app/components/HomePage.tsx
+++ app/components/HomePage.tsx
@@ -5,7 +5,6 @@ import CaseStudies from "./CaseStudies";
 import Pricing from "./Pricing";
 import Contact from "./Contact";
 import Footer from "./Footer";
-import "../styles/globals.css";
 
 const HomePage = () => {
   return (
