28f39c0d924f2a68cefb76a10ad8edacc7c63f89
ismailkhan4 <islamians96@gmail.com>
2026-08-24 11:17:46 +0500

    Updated email

--- lib/content/siteConfig.ts
+++ lib/content/siteConfig.ts
@@ -1,7 +1,7 @@
 export const calLink = "https://cal.com/heyismail/15min";
-export const githubLink = "https://github.com/heyismail";
+export const githubLink = "https://github.com/ismailkhan4";
 export const linkedinLink = "https://linkedin.com/in/heyismail";
-export const contactEmail = "ismail@heyismail.com";
+export const contactEmail = "ismaeel.kheshgi@gmail.com";
 
 export const responseTimeCopy =
   "I respond to every message myself, usually within a day.";
